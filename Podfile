6eb622a1ec9d4fc91b9b27cfd51114dbe87a02df
Slobodan Novakovic <zlatibor@gmail.com>
2018-09-29 22:17:18 +0200

    Added Google Cast and example.

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '12.0'
+
+target 'THEOPlayerDemo' do
+    use_frameworks!
+    pod 'google-cast-sdk'
+end
--- THEOPlayerDemo/ViewController.swift
+++ THEOPlayerDemo/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import THEOplayerSDK
 
 class ViewController: UIViewController {
-
+    var theoplayer: THEOplayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupTheoPlayer()
+        self.theoplayer.source = sampleSource
     }
-
-
+    
+    func setupTheoPlayer() {
+        var frame: CGRect = UIScreen.main.bounds
+        frame.origin.y = 0
+        frame.size.height = frame.size.width * 9 / 16
+        
+        self.theoplayer = THEOplayer()
+        self.theoplayer.frame =  frame
+        self.theoplayer.addAsSubview(of: self.view)
+    }
+    
+    var sampleSource: SourceDescription {
+        return SourceDescription(
+            source: TypedSource(
+                src: "https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8",
+                type: "application/x-mpegurl"
+            )
+        )
+    }
 }
-
