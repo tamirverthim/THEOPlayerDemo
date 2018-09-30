70948f7475d36812105b862c2aca7877dc0e5f57
Slobodan Novakovic <zlatibor@gmail.com>
2018-09-30 22:02:27 +0200

    Added subtitle listener and SourceType enum

--- THEOPlayerDemo/Controllers/SubtitlesController.swift
+++ THEOPlayerDemo/Controllers/SubtitlesController.swift
@@ -9,31 +9,49 @@ import UIKit
 import THEOplayerSDK
 
 class SubtitlesController: UIViewController {
-    var theoplayer: THEOplayer!
+
+    var listeners = [String: EventListener]()
+    
+    lazy var theoPlayer: THEOplayer = {
+        let tP = THEOplayer()
+        tP.source = sampleSource
+        tP.autoplay = true
+      
+        return tP
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTheoPlayer()
-        self.theoplayer.source = sampleSource
+        setupUI()
     }
     
-    func setupTheoPlayer() {
+    func setupUI() {
         view.backgroundColor = .white
+
         var frame: CGRect = UIScreen.main.bounds
         frame.origin.y = 0
         frame.size.height = frame.size.width * 9 / 16
         
-        self.theoplayer = THEOplayer()
-        self.theoplayer.frame =  frame
-        self.theoplayer.addAsSubview(of: self.view)
+        theoPlayer.frame =  frame
+        theoPlayer.addAsSubview(of: self.view)
+        
+        attachEventListeners()
     }
     
     var sampleSource: SourceDescription {
         return SourceDescription(
             source: TypedSource(
                 src: "https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8",
-                type: "application/x-mpegurl"
+                type: SourceType.mp4.rawValue
             )
         )
     }
+    
+    func attachEventListeners() {
+        
+        self.listeners["subtitleChange"] = theoPlayer.audioTracks.addEventListener(type: TextTrackListEventTypes.CHANGE) { (eventProtocol) in
+            print("Subtitle Changed:", eventProtocol.track.label)
+        }
+    }
+
 }
---
+++ THEOPlayerDemo/New Group/SourceType.swift
@@ -0,0 +1,13 @@
+//
+//  SourceType.swift
+//  THEOPlayerDemo
+//
+//  Created by Slobodan Novakovic on 9/30/18.
+//  Copyright © 2018 Slobo. All rights reserved.
+//
+
+
+enum SourceType: String {
+    case mp4 = "video/mp4"
+    case mpegURL = "application/x-mpegURL"
+}
